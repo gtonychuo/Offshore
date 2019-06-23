895df2f4c86623e1ad14963a9b7e25d5a18a4965
Chang Chia Ming <skypc785308@gmail.com>
2019-06-23 20:58:19 +0800

    lda result

--- topic model/topicmodel.R
+++ topic model/topicmodel.R
@@ -1,11 +1,12 @@
 # library(topicmodels)
-# library(tm)
+library(tm)
 # library(reshape2)
 options(digits = 10)
 library(dplyr)
 library(readr)
 library(udpipe)
 library(text2vec)
+library(ggplot2)
 
 offshore = read_csv("./topic model/offshore_wind_pos.csv")
 
@@ -15,9 +16,18 @@ offshore = read_csv("./topic model/offshore_wind_pos.csv")
 #     f = n()
 #   ) %>% dcast(title ~ text,value.var = "f")
 clean_offshore  = offshore %>% filter(pos_ %in% c("PROPN","NOUN") & is_stop == F)
-x <- document_term_frequencies(clean_offshore,document = "title", term = "lemma_")
+
+clean_offshore  = offshore %>% filter(!text %in% stopwords() & is_stop == F & pos_ %in% c("PROPN","NOUN"))
+
+x <- document_term_frequencies(offshore,document = "title", term = "lemma_")
 dtm = document_term_matrix(x)
-dtm_clean <- dtm_remove_lowfreq(dtm, minfreq = 5)
+dim(dtm)
+
+
+dtm_clean <- dtm_remove_lowfreq(dtm, minfreq = 20)
+dim(dtm_clean)
+
+dtm_clean <- dtm_remove_tfidf(dtm_clean,cutoff = 0.03)
 dim(dtm_clean)
 # dtm_clean <- dtm_remove_tfidf(dtm_clean, top=50)
 # dim(dtm_clean)
@@ -27,16 +37,18 @@ dim(dtm_clean)
 #          control = list(nstart = 5, burnin = 2000, best = TRUE, seed = 1:5))
 # m = LDA(dtm_clean,k = 10,control = list(seed=1234))
 # m
-lda_model = LDA$new(n_topics = 10, doc_topic_prior = 0.1, topic_word_prior = 0.01)
+lda_model = LDA$new(n_topics = 24, doc_topic_prior = 0.1, topic_word_prior = 0.01)
 doc_topic_distr = 
   lda_model$fit_transform(x = dtm_clean, n_iter = 1000, 
                           convergence_tol = 0.001, n_check_convergence = 25, 
                           progressbar = FALSE)
 # 取得個主題前10重要的字
-lda_model$get_top_words(n = 10, topic_number = 1:10, lambda = 1)
+lda_model$get_top_words(n = 20, lambda = 1)
+
+c =lda_model$topic_word_distribution
 
 # 取得視覺化的結果
-lda_model$plot()
+lda_model$plot(out.dir = './LDA_nn/LDAvis25')
 servr::daemon_stop(1)
 # 輸入doc index 可以得知文件屬於最高機率的主題
 which.max(doc_topic_distr[1,])
@@ -46,6 +58,32 @@ barplot(doc_topic_distr[1, ], xlab = "topic",
         names.arg = 1:ncol(doc_topic_distr))
 
 # 複雜度計算，越低越好
-perplexity(dtm_clean, topic_word_distribution = lda_model$topic_word_distribution,
+perplexity(dtm, topic_word_distribution = lda_model$topic_word_distribution,
            doc_topic_distribution = doc_topic_distr)
+
+
+perplexity_df <- data.frame(perplexity=numeric())
+topics <- c(1:25)
+set.seed(12345)
+for (i in topics){
+  lda_model = 
+    LDA$new(n_topics = i, 
+            doc_topic_prior = 0.1, topic_word_prior = 0.01)
+  doc_topic_distr = 
+    lda_model$fit_transform(dtm_clean, n_iter = 1000, convergence_tol = 0.01, 
+                            check_convergence_every_n = 10)
+  topic_word_distr_10 = lda_model$topic_word_distribution
+  perplexity_df[i,1] <-perplexity(dtm_clean, topic_word_distr_10, doc_topic_distr)
+}
+perplexity_df$log_per = log(perplexity_df$perplexity)
+g <- ggplot(data=perplexity_df, aes(x= as.numeric(row.names(perplexity_df)))) +
+  labs(y="Perplexity",x="Number of topics") +
+  ggtitle("Perplexity of data") +
+  geom_line(aes(y=log_per), colour="red")
+g
+
+hc = doc_topic_distr %>% dist%>% hclust(method = "ward.D2")
+plot(hc)
+rect.hclust(hc, k=16, border="red")
+
 
